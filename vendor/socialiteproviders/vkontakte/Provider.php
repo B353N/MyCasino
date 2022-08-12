e3d4c252e86da9b21a2c805fc6424258d6a9a3da
B353S <r.slavov.work@gmail.com>
2022-08-12 23:19:26 +0300

    Initial commit

---
+++ vendor/socialiteproviders/vkontakte/Provider.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace SocialiteProviders\VKontakte;
+
+use Illuminate\Support\Arr;
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    protected $fields = ['id', 'email', 'first_name', 'last_name', 'screen_name', 'photo_max'];
+
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'VKONTAKTE';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = ['email'];
+
+    /**
+     * Last API version.
+     */
+    const VERSION = '5.78';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://oauth.vk.com/authorize', $state
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://oauth.vk.com/access_token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $params = http_build_query([
+            'access_token' => $token,
+            'fields'       => implode(',', $this->fields),
+            'language'     => $this->getConfig('lang', 'en'),
+            'v'            => self::VERSION,
+        ]);
+
+        $response = $this->getHttpClient()->get('https://api.vk.com/method/users.get?'.$params);
+
+        $contents = $response->getBody()->getContents();
+
+        $response = json_decode($contents, true);
+
+        if (!is_array($response) || !isset($response['response'][0])) {
+            throw new \RuntimeException(sprintf(
+                'Invalid JSON response from VK: %s',
+                $contents
+            ));
+        }
+
+        return $response['response'][0];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => Arr::get($user, 'id'),
+            'nickname' => Arr::get($user, 'screen_name'),
+            'name'     => trim(Arr::get($user, 'first_name').' '.Arr::get($user, 'last_name')),
+            'email'    => Arr::get($user, 'email'),
+            'avatar'   => Arr::get($user, 'photo_max'),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+
+    /**
+     * Set the user fields to request from Vkontakte.
+     *
+     * @param array $fields
+     *
+     * @return $this
+     */
+    public function fields(array $fields)
+    {
+        $this->fields = $fields;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function additionalConfigKeys()
+    {
+        return ['lang'];
+    }
+}
